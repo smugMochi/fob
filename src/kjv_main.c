0f46928778c044b96dcae4769d5fd7b8e29bb929
smugMochi <alexid96@gmail.com>
2022-02-13 20:15:05 -0500

    Change prompt from kjv> to fob>

--- src/kjv_main.c
+++ src/kjv_main.c
@@ -129,7 +129,7 @@ main(int argc, char *argv[])
     if (argc == optind) {
         using_history();
         while (true) {
-            char *input = readline("kjv> ");
+            char *input = readline("fob> ");
             if (input == NULL) {
                 break;
             }
